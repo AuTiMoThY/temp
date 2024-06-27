bbbc5de5a367a7149ab41728e9803a9886b9f814
Ozzy <m62466@gmail.com>
2024-06-27 11:59:49 +0800

    按鈕元件調整

--- public/assets/js/components/TwBtn.js
+++ public/assets/js/components/TwBtn.js
@@ -1,10 +1,17 @@
 const TwBtn = {
     props: {
-
+        href: String,
     },
     template: `
-        <button class="tw_btn">
-            <slot></slot>
+        <a v-if="href" :href="href" class="tw_btn">
+            <span class="tw_btn-inner">
+                <slot></slot>
+            </span>
+        </a>
+        <button class="tw_btn" v-else>
+            <span class="tw_btn-inner">
+                <slot></slot>
+            </span>
         </button>
-    `
+    `,
 };
